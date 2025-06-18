8b7cef744579b80e1d3377dddfa51ef50d0eaa6f
Njau Wangari <njauwangari6@gmial.com>
2025-06-18 23:31:48 +0300

    Late commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'IDS',
+  description: 'Njaus',
+  generator: 'project',
 }
 
 export default function RootLayout({
